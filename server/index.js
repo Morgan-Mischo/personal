b819a6bda6437edcc236c7ee9a92442134cc81a5
Morgan-Mischo <goramorgan@gmail.com>
2019-07-22 13:17:30 -0600

    put in user endpoints

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require ('dotenv').config({path: __dirname + '/../.env'});
 const express = require('express'); 
 const massive = require('massive'); 
 const session = require('express-session'); 
+const uc = require('./controllers/userController'); 
 const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env; 
 
 const app = express(); 
@@ -16,5 +17,10 @@ app.use(
 ); 
 
 massive(CONNECTION_STRING).then(db => app.set('db', db)); 
+
+//user endpoints
+app.post('/api/login', uc.login); 
+app.post('/api/signup', uc.signup); 
+app.delete('/api/logout', uc.logout); 
 
 app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`)); 
