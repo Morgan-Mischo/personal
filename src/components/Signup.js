dd8312fe086199f0bd2d4f71022451e33b4044c4
Morgan-Mischo <goramorgan@gmail.com>
2019-07-22 16:26:47 -0600

    updated routes, dashboard, post, posts, signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'; 
 import { connect } from 'react-redux'; 
 import { signup } from '../redux/userReducer'; 
+import { Redirect } from 'react-router-dom'; 
 
 class Signup extends Component {
     constructor() {
@@ -25,6 +26,8 @@ class Signup extends Component {
 
     render(){
         let { username, password, first_name, last_name, email} = this.state; 
+        let { user } = this.props; 
+        if (user.loggedIn) return <Redirect to="/" />
         return (
             <div className="display-container">
                 <div className="box-medium">
