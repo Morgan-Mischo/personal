3b7c744e73eab9d386d0a7d128a9d0105d785fae
Morgan-Mischo <goramorgan@gmail.com>
2019-07-22 13:05:39 -0600

    first commit

---
+++ db/edit_post.sql
@@ -0,0 +1,6 @@
+update posts 
+set height = $2, weight = $3, calories = $3, diet = $4, workout = $5, goals = $6
+where id = $1; 
+
+select * from posts 
+where user_id = $7; 
---
+++ db/init.sql
@@ -0,0 +1,48 @@
+CREATE TABLE "users" (
+	"id" serial NOT NULL,
+	"first_name" TEXT NOT NULL,
+	"last_name" TEXT NOT NULL,
+	"username" TEXT NOT NULL,
+	"email" TEXT NOT NULL,
+	"password" TEXT NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "posts" (
+	"id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	"height" TEXT,
+	"weight" TEXT,
+	"calories" TEXT,
+	"diet" TEXT,
+	"workout" TEXT,
+	"goals" TEXT,
+	"photo" TEXT,
+	CONSTRAINT "posts_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "follow" (
+	"id" serial NOT NULL,
+	"user_following" integer NOT NULL,
+	"user_followed" integer NOT NULL,
+	CONSTRAINT "follow_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "posts" ADD CONSTRAINT "posts_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+
+ALTER TABLE "follow" ADD CONSTRAINT "follow_fk0" FOREIGN KEY ("user_following") REFERENCES "users"("id");
+ALTER TABLE "follow" ADD CONSTRAINT "follow_fk1" FOREIGN KEY ("user_followed") REFERENCES "users"("id");
+
