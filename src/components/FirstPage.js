eb40df6281ab36ebd797c317c624905bd345e59f
Morgan-Mischo <goramorgan@gmail.com>
2019-07-30 20:09:44 -0600

    styled first page

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react'; 
 import { Link } from 'react-router-dom'; 
 import { connect } from 'react-redux'; 
+import '../styling/firstPage.css'; 
+import '../styling/reset.css'
+
+
 
 class FirstPage extends Component {
     
     render() {
         return (
-            
             < div className='first-page-display'>
+                <div className='first-page-box'>
+                <style>
+@import url('https://fonts.googleapis.com/css?family=Raleway&display=swap');
+</style>
+                    <div className='app-name' >Fitbook</div>
+                    <div>
                 <Link to={{ pathname: '/login'}}>
-                <button className='login'>
+                <button className='button'>
                     Login
                 </button>
                 </Link>
+                </div>
+                <div>
                 <Link to={{ pathname: '/signup'}}>
-                <button className='signup'>
+                <button className='button'>
                     Signup
                 </button>
                 </Link>
+                </div>
             </div>
+            </div>
+            
         )
     }
 }
