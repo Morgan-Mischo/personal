2c876ee162211d1bf71158b67042908e80bd0cbc
Morgan-Mischo <goramorgan@gmail.com>
2019-07-31 10:50:27 -0600

    altered dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,51 +1,56 @@
-import React, { Component } from 'react'; 
-import { Redirect, Link } from 'react-router-dom'; 
-import { connect } from 'react-redux'; 
-import { getUser } from '../redux/userReducer'; 
-import Posts from './Posts'; 
-import Followed from './Followed'; 
-import '../styling/reset.css'; 
-import '../styling/dashboard.css'; 
+import React, { Component } from "react";
+import { Redirect, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { getUser } from "../redux/userReducer";
+import Followed from "./Followed"; 
+import "../styling/reset.css";
+import "../styling/dashboard.css";
+
 
 class Dashboard extends Component {
-    componentDidMount(){
-        if(!this.props.user.loggedIn) {
-            this.props.getUser(); 
-        } 
+  componentDidMount() {
+    if (!this.props.user.loggedIn) {
+      this.props.getUser();
     }
-    render(){
-        let { user, error, redirect } = this.props; 
-        
-        if(error || redirect) return <Redirect to="/firstpage"/>
-        if(!user.loggedIn) return <div>Loading</div>
-        return(
-            <div className='display-container-dash'>
-                          <style>
-            @import
-            url('https://fonts.googleapis.com/css?family=Raleway&display=swap');
-          </style>
-                <div className='box-medium-dash'>
-                <div className="app-name-dash">Fitbook</div>
-                <h3>Dashboard</h3>
-                <Followed />
-                <Link to={{ pathname: '/search'}}>
-                <button className="search">Search</button>
-                </Link>
-                <Link to={{ pathname: `/profile/${this.props.user.id}`}}>
-                <button className="profile">Profile</button>
-                </Link>
-                <Link to={{ pathname: '/create'}}>
-                <button className="post">Post</button>
-                </Link>
-            </div>
-            </div>
-        ); 
-    }
+  }
+  render() {
+    let { user, error, redirect } = this.props;
+
+    if (error || redirect) return <Redirect to="/firstpage" />;
+    if (!user.loggedIn) return <div>Loading</div>;
+    return (
+      <div className="display-container-dash">
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css?family=Raleway&display=swap');
+        </style>
+
+        <div className="header-bar">
+          <div className="app-name-dash">Fitbook</div>
+          <Link to={{ pathname: "/search" }}>
+            <button className="buttons-dash">Search</button>
+          </Link>
+          <Link to={{ pathname: `/profile/${this.props.user.id}` }}>
+            <button className="buttons-dash">Profile</button>
+          </Link>
+          <Link to={{ pathname: "/create" }}>
+            <button className="buttons-dash-post">Post</button>
+          </Link>
+    
+        </div>
+
+        <div className="box-medium-dash">
+          <Followed />
+        </div>
+      </div>
+    );
+  }
 }
 function mapStateToProps(state) {
-    return state.user; 
+  return state.user;
 }
 
 export default connect(
-    mapStateToProps, 
-    { getUser }) (Dashboard); 
+  mapStateToProps,
+  { getUser }
+)(Dashboard);
